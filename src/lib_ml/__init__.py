e32643e36b416e3b75293f12d195403688adfb16
jorndijk <62614275+jorndijk@users.noreply.github.com>
2025-05-03 17:37:06 +0200

    2 automate package release (#5)
    * Add minimal setup for package releasing
    
    * Upload package using twine
    
    * Fix yml indentation error
    
    * Change to using twine
    
    * Add release pipeline
    
    * Fix the versioning naming
    
    * Improve versioning in pipeline
    
    * Add space between steps

--- src/lib_ml/__init__.py
+++ src/lib_ml/__init__.py
@@ -1,7 +1,9 @@
+from .placeholder import test_package
+
 try:
     from ._version import version as __version__
 except ImportError:
     __version__ = "0.0.0" 
 
 
-__all__ = ['__version__']
+__all__ = ['__version__', 'test_package']
---
+++ src/lib_ml/placeholder.py
@@ -0,0 +1,5 @@
+
+
+def test_package():
+    """A simple test function to confirm the package is installable and callable."""
+    print("ML package is working correctly.")
